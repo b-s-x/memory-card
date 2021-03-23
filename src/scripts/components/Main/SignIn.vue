675cf9c4d740c5f424cbf0325c9cd71e70964ec0
b-s-x <b.s.x.mars@gmail.com>
2021-03-23 16:24:34 +0300

    validation input's fields

--- src/scripts/components/Main/SignIn.vue
+++ src/scripts/components/Main/SignIn.vue
@@ -1,15 +1,39 @@
 
 <template>
   <div class="container-form">
-    <form action="#" class="form" @submit.prevent>
+    <form action="#" class="form" @submit.prevent="" novalidate>
       <div class="form-title">Sign In</div>
-      <input type="email" placeholder="Email" class="input" />
       <input
+        @blur="$v.formLogSignIn.email.$touch()"
+        :class="status($v.formLogSignIn.email)"
+        v-model="formLogSignIn.email"
+        type="text"
+        placeholder="Email"
+        class="input"
+      />
+
+      <div v-if="$v.formLogSignIn.email.$dirty && !$v.formLogSignIn.email.required" class="formErrText">
+        {{ msgRequired }}
+      </div>
+
+      <div v-if="$v.formLogSignIn.email.$dirty && !$v.formLogSignIn.email.email" class="formErrText">
+        {{ msgToBeEmail }}
+      </div>
+
+      <input
+        @blur="$v.formLogSignIn.password.$touch()"
+        :class="status($v.formLogSignIn.password)"
+        v-model="formLogSignIn.password"
         type="password"
         placeholder="Password"
         autocomplete="on"
         class="input"
       />
+
+      <div v-show="$v.formLogSignIn.password.$dirty && !$v.formLogSignIn.password.minLength" class="formErrText">
+        {{ msgNotLessSymbol }}
+      </div>
+
       <a href="#" class="link">Forgot your password?</a>
 
       <m-button text="Sign In" />
@@ -20,9 +44,49 @@
 <script>
 import MButton from "@components/Button/MButton";
 
+import {
+  required,
+  email,
+  minLength,
+  sameAs,
+} from "vuelidate/dist/validators.min.js";
+
 export default {
+  data() {
+    return {
+      msgRequired: "Это поле обязательно для заполнения",
+      msgToBeEmail: "Это поле должно быть email адресом",
+      msgNotLessSymbol: "Должно быть не менее 6 символов",
+
+      formLogSignIn: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+
+  validations: {
+    formLogSignIn: {
+      email: {
+        required,
+        email,
+      },
+      password: {
+        minLength: minLength(6),
+      },
+    },
+  },
+
   components: {
     MButton,
+  },
+
+  methods: {
+    status(validator) {
+      return {
+        formErr: validator.$error,
+      };
+    },
   },
 };
 </script>
@@ -55,4 +119,14 @@ export default {
 .container-form {
   @extend %container-form;
 }
+
+.formErr {
+  background: rgba(243, 78, 78, 0.342);
+  border: 1px solid red;
+}
+
+.formErrText {
+  color: red;
+}
+
 </style>
--- src/scripts/components/Main/SignUp.vue
+++ src/scripts/components/Main/SignUp.vue
@@ -1,15 +1,57 @@
 <template>
   <div class="container-form container__signup">
-    <form action="#" class="form" @submit.prevent>
+    <form action="#" class="form" @submit.prevent="" novalidate>
       <div class="form-title">Sign Up</div>
-      <input type="text" placeholder="User" class="input" />
-      <input type="email" placeholder="Email" class="input" />
       <input
-        type="password"
-        placeholder="Password"
-        autocomplete="on"
+        @blur="$v.formRegSignUp.email.$touch()"
+        :class="status($v.formRegSignUp.email)"
+        v-model="formRegSignUp.email"
+        type="text"
+        placeholder="Email"
         class="input"
       />
+
+      <div
+        v-if="$v.formRegSignUp.email.$dirty && !$v.formRegSignUp.email.required"
+        class="formErrText"
+      >
+        {{ msgRequired }}
+      </div>
+
+      <div
+        v-if="$v.formRegSignUp.email.$dirty && !$v.formRegSignUp.email.email"
+        class="formErrText"
+      >
+        {{ msgToBeEmail }}
+      </div>
+
+      <input
+        @blur="$v.formRegSignUp.password.$touch()"
+        :class="status($v.formRegSignUp.password)"
+        v-model="formRegSignUp.password"
+        type="password"
+        placeholder="Password"
+        class="input"
+        autocomplete
+      />
+
+      <div v-show="$v.formRegSignUp.password.$dirty && !$v.formRegSignUp.password.minLength" class="formErrText">
+        {{ msgNotLessSymbol }}
+      </div>
+
+      <input
+        @blur="$v.formRegSignUp.passwordConfirm.$touch()"
+        :class="status($v.formRegSignUp.passwordConfirm)"
+        v-model="formRegSignUp.passwordConfirm"
+        type="password"
+        placeholder="Confirm password"
+        autocomplete
+        class="input"
+      />
+
+      <div v-show="$v.formRegSignUp.passwordConfirm.$dirty && !$v.formRegSignUp.passwordConfirm.sameAs" class="formErrText">
+        {{ msgNotSameAs }}
+      </div>
 
       <m-button class="btn-sign-up" text="Sign Up" />
     </form>
@@ -19,9 +61,54 @@
 <script>
 import MButton from "@components/Button/MButton";
 
+import {
+  required,
+  email,
+  minLength,
+  sameAs,
+} from "vuelidate/dist/validators.min.js";
+
 export default {
+  data() {
+    return {
+      msgRequired: "Это поле обязательно для заполнения",
+      msgToBeEmail: "Это поле должно быть email адресом",
+      msgNotLessSymbol: "Должно быть не менее 6 символов",
+      msgNotSameAs: "Пароли не совпадают",
+
+      formRegSignUp: {
+        email: "",
+        password: "",
+        passwordConfirm: "",
+      },
+    };
+  },
+
   components: {
     MButton,
+  },
+
+  validations: {
+    formRegSignUp: {
+      email: {
+        required,
+        email,
+      },
+      password: {
+        minLength: minLength(6),
+      },
+      passwordConfirm: {
+        sameAs: sameAs("password"),
+      },
+    },
+  },
+
+  methods: {
+    status(validator) {
+      return {
+        formErr: validator.$error,
+      };
+    },
   },
 };
 </script>
@@ -51,5 +138,14 @@ export default {
 
 .container__signup {
   transform: translateX(100%);
+}
+
+.formErr {
+  background: rgba(243, 78, 78, 0.342);
+  border: 1px solid red;
+}
+
+.formErrText {
+  color: red;
 }
 </style>
